c1b9a9e4f78260c577e04d8aee372ef75f8e5186
Sergey Pavlyuchenko <drubl@ya.ru>
2020-04-06 18:37:33 +0300

    initial commit

---
+++ pavlyuchenko/ozon/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField(max_length=512)
+    price = models.IntegerField()
+    first_image = models.CharField(max_length=256)
+    second_image = models.CharField(max_length=256)
+    weight = models.IntegerField()
+
+
---
+++ pavlyuchenko/ozon/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+class ProductSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=256)
+    description = serializers.CharField(max_length=512)
+    price = serializers.IntegerField()
+    firstPhoto = serializers.CharField(max_length=256, source='first_image')
+    secondPhoto = serializers.CharField(max_length=256, source='second_image')
+    weight = serializers.IntegerField()
+
