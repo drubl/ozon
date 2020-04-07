8e2c1616ef922100f03b33fdfaf1cb3bf79fec19
Sergey Pavlyuchenko <drubl@ya.ru>
2020-04-07 18:51:57 +0300

    Add all models

--- pavlyuchenko/ozon/api/models.py
+++ pavlyuchenko/ozon/api/models.py
@@ -8,8 +8,8 @@ class Product(models.Model):
     second_image = models.CharField(max_length=256)
     weight = models.IntegerField()
 
-    def __repr__(self):
-        return f'<Product {self.title}>'
+    def __str__(self):
+        return f'<Продукт {self.title}>'
 
 class Customer(models.Model):
     #login = models.CharField('Логин', max_length=64, unique=True)
@@ -20,5 +20,30 @@ class Customer(models.Model):
     birthday = models.DateField('Пароль')
     gender = models.CharField('Пол', max_length=32)
 
-    def __repr__(self):
-        return f'<Costumer {self.name}>'
+    def __str__(self):
+        return f'<Покупатель {self.name}>'
+
+
+class Cart(models.Model):
+    purchases = models.ManyToManyField(Product, through='Purchase', through_fields=('cart','product'))
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE) #корзины many-to-one
+    is_checkout = models.BooleanField('Оформлена') #Оформленная ли корзина
+
+    def __str__(self):
+        return f'<Корзина {self.customer.name}>'
+
+
+class Purchase(models.Model):
+    # Одна покупка может быть только в одной корзине. Покупка это товар и его количество
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    count = models.IntegerField('Количество')
+
+    def get_price(self):
+        return self.product.price * self.count
+    
+    def get_weight(self):
+        return self.product.weight * self.count
+
+    def __str__(self):
+        return f'<Покупка {self.product.title} в корзине {self.cart.customer.name} {self.count} штук>'
