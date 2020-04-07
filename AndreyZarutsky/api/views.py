d5edef91959f92e89e723e3d3c34661d77e75f0a
andreyklg <and.reyz@yandex.ru>
2020-04-07 10:15:16 +0300

    api/products

---
+++ AndreyZarutsky/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+from products.models import Products
+from .serializers import ProductsSerializer
+
+
+class ProductsView(APIView):
+    def get(self, request):
+        products = Products.objects.all()
+        serializer = ProductsSerializer(products, many=True)
+        return Response({'products': serializer.data})
