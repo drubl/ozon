0913a19f6227ebe3900f5d46159b45da1f52175f
andreyklg <and.reyz@yandex.ru>
2020-04-15 17:39:17 +0300

    pre alfa reg log session

--- AndreyZarutsky/api/urls.py
+++ AndreyZarutsky/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ProductsView,ProductsSet, ProductDetailView, CustomerCreate, CustomerDetailView, \
-    BasketShow, AddRemUpdOrder, Processed
+    BasketShow, AddRemUpdOrder, Processed, RegisterCusomer, \
+    create_customer_test, LoginCustomer, TESTPermission
 
 
 urlpatterns = [
@@ -11,5 +12,9 @@ urlpatterns = [
     path('customer/<int:pk>/', CustomerDetailView.as_view()),
     path('customer/<int:user_id>/cart/', BasketShow.as_view()),
     path('customer/<int:user_id>/cart/<int:product_id>', AddRemUpdOrder.as_view()),
-    path('customer/<int:user_id>/cart/checkout', Processed.as_view())
+    path('customer/<int:user_id>/cart/checkout', Processed.as_view()),
+    path('customer/create', RegisterCusomer.as_view()),
+    path('login', LoginCustomer.as_view()),
+    path('testing/email_valid', create_customer_test.as_view()),
+    path('testing/permission', TESTPermission.as_view())
 ]
