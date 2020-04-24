d51da999f0e1c4ad55fdf44b2cb5453f4d4b9e19
Sergey Pavlyuchenko <drubl@ya.ru>
2020-04-24 13:56:33 +0300

    Add category api

--- pavlyuchenko/ozon/api/category/presentation/serializers.py
+++ pavlyuchenko/ozon/api/category/presentation/serializers.py
@@ -1,24 +1,7 @@
 from rest_framework import serializers
-from api.product.infrastructure.models import Product
+from api.category.infrastructure.models import Category
 
-class ProductSerializer(serializers.ModelSerializer):
-    firstPhoto = serializers.CharField(max_length=256, source='first_image')
-    secondPhoto = serializers.CharField(max_length=256, source='second_image')
-
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Product
-        fields = ['id', 'title', 'description', 'price', 'firstPhoto', 'secondPhoto', 'weight']
-
-    def create(self, validated_data):
-        return Product.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.price = validated_data.get('price', instance.price)
-        instance.first_image = validated_data.get('firstPhoto', instance.first_image)
-        instance.second_image = validated_data.get('secondPhoto', instance.second_image)
-        instance.weight = validated_data.get('weight', instance.weight)
-
-        instance.save()
-        return instance
+        model = Category
+        fields = ['title', 'slug', 'description']
--- pavlyuchenko/ozon/api/category/presentation/views.py
+++ pavlyuchenko/ozon/api/category/presentation/views.py
@@ -4,13 +4,16 @@ from rest_framework.permissions import AllowAny
 
 # from api.product.infrastructure.models import Product
 from api.category.domain.logic import get_category_products, filtering, sorting
+from api.category.infrastructure.models import Category
 from api.product.presentation.serializers import ProductSerializer
+from .serializers import CategorySerializer
 
-# class CategoryView(APIView):
-#     permission_classes = [AllowAny]
-#     def get(self, request):
-#         categories = Category.objects.all()
-#         return categories
+class CategoryView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response({'categories': serializer.data})
 
 class CategoryDetailView(APIView):
     permission_classes = [AllowAny]
